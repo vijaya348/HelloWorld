2c2817878a628ac35da1273051b331961da70078
vijaya348 <68677321+vijaya348@users.noreply.github.com>
2020-07-27 18:04:54 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,6 +1,8 @@
 package hello;
-public class HelloWorld{
+
+public class HelloWorld {
   public static void main(String[] args) {
-        System.out.println("Hello All");
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
   }
 }
